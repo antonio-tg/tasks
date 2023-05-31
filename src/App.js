23465b5dd517350b8ffc980530a4bd251fe41dae
José Antonio Trejo Gutiérrez <jose.trejog@sat.gob.mx>
2023-05-30 19:19:36 -0600

    Add style to the main div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import FreeCodeCamp_Logo from './images/fcc_primary_large.jpg'
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className="task-app">
+      <div className='freecode_camp_logo_container'>
+        <img
+          src={FreeCodeCamp_Logo}
+          className='freecode_camp_logo' />
+      </div>
+      <div className='main_task_list' >
+        <h1>Mis Tareas</h1>
+      </div>
     </div>
   );
 }
